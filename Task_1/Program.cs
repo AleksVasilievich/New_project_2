d197e7879b59161f417146c2dff87e514c1af6b1
Aleksandr <b.a.v.7575@mail.ru>
2022-05-15 23:47:35 +0300

    Упростили код Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,14 +5,10 @@ int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число -> ");
 int num2 = int.Parse(Console.ReadLine());
 int result = num2 * num2;
-if (result == num1)
-{
-    Console.Write("Первое число является квадратом второго");
-}
-else
-{
-    Console.Write("Первое число не является квадратом второго");
-}
+if (result == num1) Console.Write("Первое число является квадратом второго");
+
+else Console.Write("Первое число не является квадратом второго");
+
 
 //bool result = num2 * num2 == num1;
 //Console.WriteLine(result "Да" ? : "Нет");
