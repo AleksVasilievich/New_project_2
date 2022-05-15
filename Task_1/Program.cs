3f051b6a3ab2c7da7f87f73e8a90bcd189f94753
Aleksandr <b.a.v.7575@mail.ru>
2022-05-15 23:58:26 +0300

    Исправление в Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Напишите программу,которая на вход принимает два числа 
 // и проверяет,является ли первое число квадратом второго.
+
 Console.Write("Введите первое число -> ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число -> ");
 int num2 = int.Parse(Console.ReadLine());
 int result = num2 * num2;
 if (result == num1) Console.Write("Первое число является квадратом второго");
-
 else Console.Write("Первое число не является квадратом второго");
 
 
