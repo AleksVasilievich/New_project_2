5d7d0294b029e1d9a5a7290addb574651d58efd1
Aleksandr <b.a.v.7575@mail.ru>
2022-05-19 20:46:01 +0300

    Методы

---
+++ Task_L3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу ,которая принимает на вход число(A)
+// и выдаёт сумму чисел от 1до  A.
+// 7 -> 28
+Console.WriteLine(" Введите натуральное число ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSumNambers(int namber)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+int sumResult = GetSumNambers(num);
+
+Console.WriteLine($"Сумма чисел от 1 до {num} = {GetSumNambers(num)}");
+
+//int sumResult = GetSumNambers(num);
+
+
+// int sum = 0;
+// for (int i = 1; i <= num; i++)
+// {
+//     sum = sum + i;
+// }
+// Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
